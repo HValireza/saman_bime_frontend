d18490e372ad20de5546ac26b282191ef294f24b
Ashkan Tofangdar <ashkan4472@gmail.com>
2022-12-23 05:36:21 +0330

    Added about up static contents

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,5 +1,50 @@
-const About = () => {
-  return <div>About</div>;
+import { AboutUsTemplate } from "../../components/templates/AboutUsTemplate";
+import Texts from "../../assets/texts/about_us.json";
+import History from "../../assets/images/about_us/history.jpg";
+import Goals from "../../assets/images/about_us/goals.jpg";
+import Avail from "../../assets/images/about_us/avail.jpg";
+import Worldview from "../../assets/images/about_us/worldview.jpg";
+
+interface AboutPropsI {
+  state: "history" | "goals" | "avail" | "worldview";
+}
+
+const About = ({ state }: AboutPropsI) => {
+  const data = [
+    {
+      state: "history",
+      description: Texts.history,
+      pic: History,
+      title: "تاریخچه شرکت بیمه اتکایی سامان",
+    },
+    {
+      state: "goals",
+      description: Texts.goals,
+      pic: Goals,
+      title: "اهداف راهبردی، مأموریت و چشم‌انداز",
+    },
+    {
+      state: "avail",
+      description: Texts.avail,
+      pic: Avail,
+      title: "ارزش‌های محوری",
+    },
+    {
+      state: "worldview",
+      description: Texts.worldview,
+      pic: Worldview,
+      title: " بیانیه مأموریت و جهان‌بینی",
+    },
+  ];
+
+  const item = data.find((x) => x.state === state);
+  return (
+    <AboutUsTemplate
+      pic={item!.pic}
+      title={item!.title}
+      description={item!.description}
+    />
+  );
 };
 
 export default About;
