0fceb99874f33c30cbc1009b7931bb5eae6994d6
Hosein Hoseiny <seied78@gmail.com>
2022-12-27 17:39:13 +0330

    news routings added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { NewsData } from "./components/templates/News/data/NewsTemplateData";
+import NewsTemplate from "./components/templates/News/NewsTemplate";
+import { NewsEnum } from "./global/news.enum";
 import About from "./pages/about/about";
 import Home from "./pages/home/home";
+import News from "./pages/news/News";
 import Layout from "./pages/templates/layout";
 import "./styles/App.scss";
 
@@ -15,6 +19,24 @@ function App() {
             <Route path="goals" element={<About state={"goals"} />} />
             <Route path="avail" element={<About state={"avail"} />} />
             <Route path="worldview" element={<About state={"worldview"} />} />
+          </Route>
+          <Route path="news">
+            <Route
+              path="company"
+              element={<News data={NewsData} state={NewsEnum.COMPANY} />}
+            />
+            <Route
+              path="insurance"
+              element={<News data={NewsData} state={NewsEnum.INSURANCE} />}
+            />
+            <Route
+              path="shareholder"
+              element={<News data={NewsData} state={NewsEnum.SHAREHOLDER} />}
+            />
+            <Route
+              path="article"
+              element={<News data={NewsData} state={NewsEnum.ARTICLE} />}
+            />
           </Route>
         </Route>
       </Routes>
