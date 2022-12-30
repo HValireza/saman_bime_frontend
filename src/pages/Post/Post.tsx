c13994e35f3fba5aaa5f156dbb73a39cbdc41701
Hosein Hoseiny <seied78@gmail.com>
2022-12-30 19:50:33 +0330

    post page added

---
+++ src/pages/Post/Post.tsx
@@ -0,0 +1,18 @@
+import { useParams } from "react-router-dom";
+import PostTemplate from "../../components/templates/Post/PostTemplate";
+import GetPost from "./GetPost/GetPost";
+
+const PostPage: React.FC = () => {
+  const postId: any = useParams();
+  const post = GetPost(postId.id);
+
+  return (
+    <PostTemplate
+      post={post.data}
+      error={post.error}
+      loading={post.loading}
+    ></PostTemplate>
+  );
+};
+
+export default PostPage;
