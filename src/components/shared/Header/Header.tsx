b5696eeb78a569b0418adbca2302368ed9e35717
Hosein Hoseiny <seied78@gmail.com>
2023-01-15 23:44:03 +0330

    fixed header

--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,4 +1,7 @@
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styled from "styled-components";
+import uk from "../../../assets/images/vectors/uk.png";
 
 const Header: React.FC = () => {
   return (
@@ -8,7 +11,15 @@ const Header: React.FC = () => {
         <GrayTriangle></GrayTriangle>
       </Gray>
       <Blue>
-        <BlueRectangle></BlueRectangle>
+        <BlueRectangle>
+          <Flag src={uk} alt={"uk"} />
+          <SearchBox id="search-box"></SearchBox>
+          <SearchLabel htmlFor="search-box">
+            <SearchIcon>
+              <FontAwesomeIcon icon={faSearch}></FontAwesomeIcon>
+            </SearchIcon>
+          </SearchLabel>
+        </BlueRectangle>
         <BlueTriangle></BlueTriangle>
       </Blue>
     </Container>
@@ -57,6 +68,13 @@ const BlueRectangle = styled.div`
   width: 99%;
   height: 100%;
   background-color: #00009c;
+
+  display: flex;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: flex-start;
+
+  padding-left: 1rem;
 `;
 
 const BlueTriangle = styled.div`
@@ -64,4 +82,63 @@ const BlueTriangle = styled.div`
   height: 0;
   border-top: 3rem solid #00009c;
   border-right: 1rem solid transparent;
+`;
+
+const Flag = styled.img`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  cursor: pointer;
+
+  margin-right: 2rem;
+`;
+
+const SearchBox = styled.input`
+  transition: all 0.6s ease-out;
+  direction: ltr;
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  border: none;
+  cursor: pointer;
+  background: rgb(235, 235, 235);
+  &:hover {
+    color: white;
+    background: rgb(200, 200, 200);
+    box-shadow: 0 0 0 5px rgb(61, 71, 82);
+  }
+
+  &:focus {
+    transition: width 0.6s cubic-bezier(0, 1.22, 0.66, 1.39), border-radius 0.6s,
+      background 0.6s;
+    border: none;
+    outline: none;
+    box-shadow: none;
+    padding-left: 15px;
+    cursor: text;
+    width: 300px;
+    border-radius: auto;
+    background: rgb(235, 235, 235);
+    color: black;
+  }
+  &:not(:focus) {
+    text-indent: -5000px;
+  }
+`;
+
+const SearchLabel = styled.label``;
+
+const SearchIcon = styled.span`
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  left: -40px;
+  color: #000000;
+  cursor: pointer;
+  &:hover {
+    color: #000000;
+  }
 `;
