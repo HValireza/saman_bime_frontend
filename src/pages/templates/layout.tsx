143431ded4790ae092baef3ee4ee5f7f156b357a
Hosein Hoseiny <seied78@gmail.com>
2023-01-19 13:35:01 +0330

    start 10 day destruction at 2021/21/1

--- src/pages/templates/layout.tsx
+++ src/pages/templates/layout.tsx
@@ -1,3 +1,5 @@
+import moment from "jalali-moment";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import ScrollToTop from "react-scroll-to-top";
 import styled from "styled-components";
@@ -5,8 +7,20 @@ import Footer from "../../components/shared/footer/footer";
 import Header from "../../components/shared/Header/Header";
 import Navbar from "../../components/shared/navbar/navbar";
 const Layout = () => {
+  let prevDate = moment().diff(moment("2023/21/1", "YYYY/D/M"), "d") / 10;
+  const [time, setTime] = useState(prevDate);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      prevDate = moment().diff(moment("2023/21/1", "YYYY/D/M"), "d") / 10;
+      setTime(time > 1 ? 1 : prevDate);
+    }, 10000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
-    <Container>
+    <Container time={time}>
       <Header />
       <Navbar />
       <Wrapper>
@@ -20,12 +34,13 @@ const Layout = () => {
 
 export default Layout;
 
-const Container = styled.div`
+const Container = styled.div<any>`
   width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: center;
+  opacity: ${(props) => 1 - props.time};
 `;
 
 const Wrapper = styled.div`
