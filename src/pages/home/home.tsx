3cae5c42e1e9085addf13b48314feeea96aa292c
Ashkan Tofangdar <ashkan4472@gmail.com>
2023-01-30 13:36:06 +0330

    Cleanup useless styled component in home page

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -25,58 +25,11 @@ export default Home;
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: auto;
 
   display: flex;
   flex-direction: column;
   align-items: center;
 
   background-color: white;
-`;
-
-const ScrollTrigger = styled.div`
-  height: 30vh;
-  width: 1rem;
-
-  position: fixed;
-  right: 1rem;
-  top: 30vh;
-
-  z-index: 10;
-
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  gap: 2rem;
-
-  #num4::after {
-    background-color: red;
-    display: none;
-  }
-
-  button {
-    width: 0.8rem;
-    height: 0.8rem;
-
-    border-radius: 50%;
-    background-color: transparent;
-    border: 1px solid white;
-    position: relative;
-
-    ::after {
-      content: "";
-      width: 1px;
-      height: 2.1rem;
-      background-color: white;
-      position: absolute;
-      top: 0.7rem;
-      left: 50%;
-    }
-
-    :focus {
-      background-color: white;
-      outline: white;
-    }
-  }
 `;
