7dfb9d2d83df52c73c77123d269ef1378e9d3d49
Hosein Hoseiny <seied78@gmail.com>
2023-02-03 14:53:50 +0330

    Added dropdown menu

---
+++ src/components/Layout/components/Navbar/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,123 @@
+import { useRef, useState } from "react";
+import { CSSTransition } from "react-transition-group";
+import DropdownItem from "./components/DropdownItem/DropdownItem";
+import "./Dropdown.scss";
+
+import {
+  mainDropdownData,
+  IMenuData,
+} from "../../../../../../Mock/Dropdown/DropdownMock";
+
+interface IDropDown {
+  closeMenu?: (onClose: boolean) => void;
+}
+
+// todo type safety
+const Dropdown: React.FC<IDropDown> = ({ closeMenu }) => {
+  const [activeMenu, setActiveMenu] = useState("main");
+  const mainRef = useRef(null);
+  const nodeRefT = useRef(null);
+  const [secondMenuData, setSecondMenuData] = useState<IMenuData[]>([
+    { id: 0, label: "بازگشت" },
+  ]);
+  const [thirdMenuData, setThirdMenuData] = useState<IMenuData[]>([
+    { id: 0, label: "بازگشت" },
+  ]);
+
+  return (
+    <div className="si-dropdown-container">
+      {/* main dropdown */}
+
+      <CSSTransition
+        nodeRef={mainRef}
+        in={activeMenu === "main"}
+        timeout={500}
+        classNames="si-dropdown-primary"
+        unmountOnExit
+      >
+        <div className="si-dropdown" ref={mainRef}>
+          {mainDropdownData.map((d) => (
+            <DropdownItem
+              goToMenu={d.selectMenu}
+              key={d.id}
+              leftIcon={d.switchIcon}
+              mainIcon={d.mainIcon}
+              setActiveMenu={setActiveMenu}
+              address={d.address}
+              secondMenuData={d.subMenuData}
+              setSecondMenuData={setSecondMenuData}
+              closeMenu={closeMenu}
+              outerLink={d.outerLink}
+              mainIconReverse={d.mainIconReverse}
+              leftIconReverse={d.switchIconReverse}
+            >
+              {d.label}
+            </DropdownItem>
+          ))}
+        </div>
+      </CSSTransition>
+
+      {/* secondary dropdown */}
+
+      <CSSTransition
+        nodeRef={nodeRefT}
+        in={activeMenu === "secondary"}
+        timeout={500}
+        classNames="si-dropdown-secondary"
+        unmountOnExit
+      >
+        <div className="si-dropdown" ref={nodeRefT}>
+          {secondMenuData.map((s) => (
+            <DropdownItem
+              address={s.address}
+              goToMenu={s.selectMenu}
+              key={s.id}
+              leftIcon={s.switchIcon}
+              mainIcon={s.mainIcon}
+              secondMenuData={s.subMenuData}
+              setSecondMenuData={setThirdMenuData}
+              setActiveMenu={setActiveMenu}
+              closeMenu={closeMenu}
+              outerLink={s.outerLink}
+              mainIconReverse={s.mainIconReverse}
+              leftIconReverse={s.switchIconReverse}
+            >
+              {s.label}
+            </DropdownItem>
+          ))}
+        </div>
+      </CSSTransition>
+
+      {/* tertiary dropdown */}
+
+      <CSSTransition
+        nodeRef={nodeRefT}
+        in={activeMenu === "tertiary"}
+        timeout={500}
+        classNames="si-dropdown-secondary"
+        unmountOnExit
+      >
+        <div className="si-dropdown" ref={nodeRefT}>
+          {thirdMenuData.map((t) => (
+            <DropdownItem
+              address={t.address}
+              goToMenu={t.selectMenu}
+              key={t.id}
+              leftIcon={t.switchIcon}
+              mainIcon={t.mainIcon}
+              setActiveMenu={setActiveMenu}
+              closeMenu={closeMenu}
+              outerLink={t.outerLink}
+              mainIconReverse={t.mainIconReverse}
+              leftIconReverse={t.switchIconReverse}
+            >
+              {t.label}
+            </DropdownItem>
+          ))}
+        </div>
+      </CSSTransition>
+    </div>
+  );
+};
+
+export default Dropdown;
---
+++ src/components/Layout/components/Navbar/components/Dropdown/components/DropdownItem/DropdownItem.tsx
@@ -0,0 +1,79 @@
+import { useNavigate } from "react-router-dom";
+import { IMenuData } from "../../../../../../../../Mock/Dropdown/DropdownMock";
+import "./DropdownItem.scss";
+
+interface IDropdownItem {
+  address?: string;
+  goToMenu?: string;
+  mainIcon?: string;
+  leftIcon?: string;
+  outerLink?: string;
+  children?: React.ReactNode;
+  mainIconReverse?: boolean;
+  leftIconReverse?: boolean;
+  secondMenuData?: IMenuData[];
+  closeMenu?: (close: boolean) => void;
+  setActiveMenu?: (goToMenu: string) => void;
+  setSecondMenuData?: (secondMenuData: IMenuData[]) => void;
+}
+
+const DropdownItem: React.FC<IDropdownItem> = ({
+  goToMenu,
+  children,
+  leftIcon,
+  mainIcon,
+  setActiveMenu,
+  address,
+  secondMenuData,
+  setSecondMenuData,
+  closeMenu,
+  outerLink,
+  mainIconReverse,
+  leftIconReverse,
+}) => {
+  const navigate = useNavigate();
+
+  // todo fix scroll behavior on navigation
+  const selectOption = () => {
+    secondMenuData && setSecondMenuData!(secondMenuData);
+    goToMenu && setActiveMenu!(goToMenu);
+    address && closeMenu!(false);
+    address && window.scrollTo(0, 0);
+    address && navigate(address);
+    outerLink && closeMenu!(false);
+    outerLink && window.open(outerLink, "_blank");
+  };
+  return (
+    <div className="si-dropdown-item" onClick={selectOption}>
+      <span className="si-dropdown-icon-button">
+        {mainIcon && (
+          <img
+            src={mainIcon}
+            alt="dropdown icon"
+            className={
+              mainIconReverse
+                ? "si-dropdown-icon si-icon-reverse"
+                : "si-dropdown-icon"
+            }
+          />
+        )}
+      </span>
+      {children}
+      <span className="si-dropdown-icon-left">
+        {leftIcon && (
+          <img
+            src={leftIcon}
+            alt="dropdown switch icon"
+            className={
+              leftIconReverse
+                ? "si-dropdown-icon si-icon-reverse"
+                : "si-dropdown-icon"
+            }
+          />
+        )}
+      </span>
+    </div>
+  );
+};
+
+export default DropdownItem;
