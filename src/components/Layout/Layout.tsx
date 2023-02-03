c1ba8195b34c77d3c62e7229ac2d264193b78137
Hosein Hoseiny <seied78@gmail.com>
2023-02-04 01:56:07 +0330

    Added language state to layout

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -2,11 +2,14 @@ import "./Layout.scss";
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
 
 const Layout: React.FC = () => {
+  const [language, setLanguage] = useState<string>("fa");
+
   return (
     <div className="si-layout-container">
-      <Navbar />
+      <Navbar language={language} setLanguage={setLanguage} />
       <Outlet />
       <Footer />
     </div>
