bec3a7d3ec13ffcbd093a26d2335a3c829c17d8f
Hosein Hoseiny <seied78@gmail.com>
2023-02-07 23:08:53 +0330

    Added navbar bg color

--- src/components/Layout/components/Navbar/Navbar.tsx
+++ src/components/Layout/components/Navbar/Navbar.tsx
@@ -1,6 +1,7 @@
 import Burger from "./components/Burger/Burger";
 import Header from "./components/Header/Header";
 import LanguageButton from "./components/LanguageButton/LanguageButton";
+import NavbarBG from "./components/NavbarBG/NavbarBG";
 import SearchButton from "./components/SearchButton/SearchButton";
 import "./Navbar.scss";
 
@@ -12,6 +13,7 @@ interface INavbar {
 const Navbar: React.FC<INavbar> = ({ language, setLanguage }) => {
   return (
     <div className="si-navbar-container">
+      <NavbarBG />
       <Header />
       <Burger />
       <SearchButton />
---
+++ src/components/Layout/components/Navbar/components/NavbarBG/NavbarBG.tsx
@@ -0,0 +1,7 @@
+import "./NavbarBG.scss";
+
+const NavbarBG: React.FC = () => {
+  return <div className="si-navbar-bg"></div>;
+};
+
+export default NavbarBG;
