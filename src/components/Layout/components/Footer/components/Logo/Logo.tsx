b23fa21e315ff1a36192bd222d2d1e1732ddb6cc
Hosein Hoseiny <seied78@gmail.com>
2023-02-07 18:33:52 +0330

    Added footer logo component

---
+++ src/components/Layout/components/Footer/components/Logo/Logo.tsx
@@ -0,0 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import logo from "../../../../../../assets/logo/logo3.png";
+import "./Logo.scss";
+
+const Logo: React.FC = () => {
+  const navigate = useNavigate();
+  const navigation = (address?: string) => {
+    scrollTo(0, 0);
+    navigate(address ? address : "/");
+  };
+
+  return (
+    <div className="si-footer-logo-container">
+      <img
+        src={logo}
+        alt="saman reinsurance"
+        onClick={() => navigation("/")}
+        className="si-footer-logo"
+      />
+    </div>
+  );
+};
+
+export default Logo;
