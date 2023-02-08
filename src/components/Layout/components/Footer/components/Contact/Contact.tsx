6b39e3ffa62d5f66b50e45f17d5f7c75dca369cc
Hosein Hoseiny <seied78@gmail.com>
2023-02-08 23:43:01 +0330

    Added animation to contact component

--- src/components/Layout/components/Footer/components/Contact/Contact.tsx
+++ src/components/Layout/components/Footer/components/Contact/Contact.tsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import "./Contact.scss";
 
 interface IContact {
@@ -9,7 +10,13 @@ interface IContact {
 const Contact: React.FC<IContact> = ({ alt, detail, icon }) => {
   return (
     <div className="si-footer-contact-box">
-      <img src={icon} alt={alt} className="si-footer-contact-icon" />
+      <motion.img
+        src={icon}
+        alt={alt}
+        className="si-footer-contact-icon"
+        initial={{ x: 0 }}
+        whileHover={{ x: 30 }}
+      />
       <div className="si-footer-contact-line"></div>
       <div className="si-footer-contact-detail">{detail}</div>
     </div>
