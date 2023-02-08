c8f3aede39e9bab8e72052d42fe82c797427d234
Hosein Hoseiny <seied78@gmail.com>
2023-02-09 02:26:05 +0330

    Added author component

---
+++ src/components/Author/Author.tsx
@@ -0,0 +1,41 @@
+import dayjs from "dayjs";
+import jalaliday from "jalaliday";
+import logo from "../../assets/logo/logo4.png";
+import "./Author.scss";
+
+interface IAuthor {
+  thumb?: string;
+  name?: string;
+  position?: string;
+  createdAt?: string;
+  state?: string;
+}
+
+const Author: React.FC<IAuthor> = ({
+  createdAt = new Date().toISOString(),
+  name = "بیمه اتکایی سامان",
+  position = "بیمه اتکایی سامان",
+  thumb = logo,
+  state = "post",
+}) => {
+  dayjs.extend(jalaliday);
+
+  return (
+    <div className={state === "post" ? "si-post-author" : "si-author"}>
+      <div className="si-author-info">
+        <img src={thumb} alt="" className="si-author-thumb" />
+        <div className="si-author-info-wrapper">
+          <h4 className="si-author-name">{name}</h4>
+          <h5 className="si-author-position">{position}</h5>
+        </div>
+      </div>
+
+      {/* created at time */}
+      <div className="si-author-date">
+        {dayjs(createdAt).calendar("jalali").format("YYYY/MM/DD")}
+      </div>
+    </div>
+  );
+};
+
+export default Author;
