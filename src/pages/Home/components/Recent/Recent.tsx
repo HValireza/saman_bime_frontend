f5ef3bc8455723592daa86b4d9530c7f9ce82625
Hosein Hoseiny <seied78@gmail.com>
2023-02-08 20:56:53 +0330

    Added animations to recent component

--- src/pages/Home/components/Recent/Recent.tsx
+++ src/pages/Home/components/Recent/Recent.tsx
@@ -1,26 +1,57 @@
+import { motion } from "framer-motion";
 import { RecentPosts } from "../../../../Mock/Home/Recent/RecentMock";
 import RecentCard from "./components/RecentCard/RecentCard";
 import "./Recent.scss";
 
 const Recent: React.FC = () => {
+  const container = {
+    hidden: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      transition: {
+        delayChildren: 0.2,
+        staggerChildren: 0.5,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { x: 200, opacity: 0 },
+    visible: {
+      opacity: 1,
+      x: 0,
+      transition: {
+        duration: 0.5,
+      },
+    },
+  };
+
   return (
     <div className="si-recent-container">
       <h1 className="si-recent-title">مقالات، اخبار و رویداد ها</h1>
-      <div className="si-recent-card-container">
+      <motion.div
+        className="si-recent-card-container"
+        variants={container}
+        initial="hidden"
+        whileInView="visible"
+        viewport={{ once: true }}
+      >
         {RecentPosts.map((p) => (
-          <RecentCard
-            id={p.id}
-            createdAt={p.createdAt}
-            description={p.description}
-            image={p.image}
-            name={p.name}
-            position={p.position}
-            thumb={p.thumb}
-            title={p.title}
-            key={p.id}
-          />
+          <motion.div variants={item}>
+            <RecentCard
+              id={p.id}
+              createdAt={p.createdAt}
+              description={p.description}
+              image={p.image}
+              name={p.name}
+              position={p.position}
+              thumb={p.thumb}
+              title={p.title}
+              key={p.id}
+            />
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
