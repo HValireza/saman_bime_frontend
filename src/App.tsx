7194df148de0606ce0d29f97c8ca5f95a49b26cd
Hosein Hoseiny <seied78@gmail.com>
2023-02-09 02:28:39 +0330

    Added app route to post component

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import "./App.scss";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home/Home";
 import NoPage from "./pages/NoPage/NoPage";
+import Post from "./pages/Post/Post";
 
+// todo make routes dynamic
 function App() {
   return (
     <BrowserRouter>
@@ -13,6 +15,10 @@ function App() {
         <Route path="/" element={<Layout />}>
           {/* Home Page */}
           <Route index element={<Home />} />
+
+          {/* Post Page */}
+          {/* it goes for news detail or any kind of post */}
+          <Route path="post" element={<Post />} />
 
           {/* 404 */}
           <Route path="*" element={<NoPage />} />
