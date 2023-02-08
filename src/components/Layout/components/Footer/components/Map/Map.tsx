fa678176d278c20d079e417193f2dfde54483e86
Hosein Hoseiny <seied78@gmail.com>
2023-02-08 23:43:16 +0330

    Added map link

--- src/components/Layout/components/Footer/components/Map/Map.tsx
+++ src/components/Layout/components/Footer/components/Map/Map.tsx
@@ -4,8 +4,15 @@ import "./Map.scss";
 // todo replace with real map
 
 const Map: React.FC = () => {
+  const goToMap = () => {
+    window.open(
+      "https://goo.gl/maps/rSzxgjHRRb8aiUM39",
+      "_blank",
+      "location=yes"
+    );
+  };
   return (
-    <div className="si-footer-map-container">
+    <div className="si-footer-map-container" onClick={goToMap}>
       <img src={mapMockPic} alt="map" className="si-footer-map" />
     </div>
   );
