ddeef9e886a18f64e7fd0e51d6cd58d1120898f7
Hosein Hoseiny <seied78@gmail.com>
2023-02-09 11:48:38 +0330

    Added animations to banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -15,23 +15,63 @@ const Banner: React.FC<IBanner> = ({
   secondaryTitle,
   tertiaryTitle,
 }) => {
+  const container = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+        delayChildren: 0.2,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: -20, opacity: 0 },
+    visible: {
+      opacity: 1,
+      y: 0,
+      transition: {
+        duration: 0.4,
+      },
+    },
+  };
+
+  const subItem = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: {
+        duration: 0.4,
+        delayChildren: 0.2,
+        staggerChildren: 0.3,
+      },
+    },
+  };
+
   return (
     <motion.div
       className="si-banner-container"
       style={{ backgroundImage: `url(${image})` }}
+      variants={container}
+      initial="hidden"
+      animate="visible"
     >
       {mainTitle || secondaryTitle || tertiaryTitle ? (
-        <motion.div className="si-banner-title-wrapper">
+        <motion.div className="si-banner-title-wrapper" variants={subItem}>
           {mainTitle && (
-            <motion.h1 className="si-banner-main-title">{mainTitle}</motion.h1>
+            <motion.h1 className="si-banner-main-title" variants={item}>
+              {mainTitle}
+            </motion.h1>
           )}
           {secondaryTitle && (
-            <motion.h2 className="si-banner-secondary-title">
+            <motion.h2 className="si-banner-secondary-title" variants={item}>
               {secondaryTitle}
             </motion.h2>
           )}
           {tertiaryTitle && (
-            <motion.h3 className="si-banner-tertiary-title">
+            <motion.h3 className="si-banner-tertiary-title" variants={item}>
               {tertiaryTitle}
             </motion.h3>
           )}
