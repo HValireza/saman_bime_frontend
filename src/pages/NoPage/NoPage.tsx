44a66f1a0c1441dc7464211fea1efe875f6c613b
Hosein Hoseiny <seied78@gmail.com>
2023-02-10 12:28:16 +0330

    WIP NoPage component Added

---
+++ src/pages/NoPage/NoPage.tsx
@@ -0,0 +1,13 @@
+import "./NoPage.scss";
+
+// todo add styles
+const NoPage: React.FC = () => {
+  return (
+    <div className="si-no-page">
+      <div>404</div>
+      <div>صفحه مورد نظر پیدا نشد</div>
+    </div>
+  );
+};
+
+export default NoPage;
