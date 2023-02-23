6bece5d86b85fd3e8e8ab5c8d6d1a89a51ea3dd9
Hosein Hoseiny <seied78@gmail.com>
2023-02-23 13:26:25 +0330

    Fixed scroll bug on Reinsurance Component

--- src/pages/Home/components/Reinsurance/Reinsurance.tsx
+++ src/pages/Home/components/Reinsurance/Reinsurance.tsx
@@ -6,6 +6,8 @@ const Reinsurance: React.FC = () => {
     <div className="si-reinsurance-container">
       <h2 className="si-reinsurance-title">بیمه های اتکایی</h2>
       <CardSlider />
+      <div className="si-reinsurance-scroll-left"></div>
+      <div className="si-reinsurance-scroll-right"></div>
     </div>
   );
 };
