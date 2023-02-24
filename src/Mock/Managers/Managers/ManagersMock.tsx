6f90fc42cbb47768f184f34345e5c4be9ffa9d93
Hosein Hoseiny <seied78@gmail.com>
2023-02-24 07:35:35 +0330

    Added raw html to Manager mock data

--- src/Mock/Managers/Managers/ManagersMock.tsx
+++ src/Mock/Managers/Managers/ManagersMock.tsx
@@ -16,7 +16,7 @@ export const ManagersData: IManager[] = [
     name: "هادی کشاورزی شهربابکی",
     position: "مدیر بیمه‌های اتکایی",
     image: hadi,
-    data: "در حال حاضر فاقد محتوی",
+    data: '<p dir="rtl">سوابق کاری:</p><ul><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">مشاور مدیرعامل در توسعه بازار و بین&zwnj;الملل شرکت سازه&zwnj;های آلومینیومی رادبه</p></li><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">مدیر بیمه&zwnj;های اتکایی شرکت بیمه آرمان</p></li><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">مدیر بیمه&zwnj;های اتکایی شرکت بیمه دی</p></li><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">مدیر دفاتر خارج از کشور بیمه دی</p></li><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">رئیس شعبه استان البرز شرکت بیمه سامان</p></li><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">مترجم و روزنامه&zwnj;نگار</p></li></ul><p><strong>&nbsp;</strong></p><p dir="rtl">مدارک تحصیلی:</p><ul><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">کارشناسی مدیریت بیمه اکو</p></li><li dir="rtl" aria-level="1"><p dir="rtl" role="presentation">کارشناسی ارشد MBA گرایش بازاریابی</p></li></ul><p><strong>&nbsp;</strong></p><p dir="rtl">شماره تماس (داخلی): (۴۱20)۹۱۰۰۸۸۸۷ &ndash; ۰۲۱</p><p><strong id="docs-internal-guid-ddc2abbb-7fff-3116-58e0-d154d75f2334">ایمیل: <a href="mailto:m.noohi@samanre.com">&nbsp;h.keshavarzi@samanre.com</a> </strong></p>',
   },
   {
     id: 1,
