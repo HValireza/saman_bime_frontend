f66f2ceeaf3826c5c270f274aa20cde85b2b937a
Hosein Hoseiny <seied78@gmail.com>
2023-03-07 21:12:37 +0330

    Added news queries

---
+++ src/api/news/useGetAllNews.tsx
@@ -0,0 +1,23 @@
+import { useQuery } from "react-query";
+import { queryKeys } from "../../global/queryKeys";
+import { axiosInstance } from "../../helpers/axios";
+import { News } from "../../models/news";
+import { IPaginateQuery } from "../../models/paginateQuery";
+import { IPaginateResponse } from "../../models/paginateResponse";
+import { IQuerySuccessError } from "../../models/querySuccessError";
+
+const getAllNews = (params?: IPaginateQuery) => {
+  return axiosInstance.get<IPaginateResponse<News>>("/news", {
+    params,
+  });
+};
+
+export const useGetAllNews = (
+  params?: IPaginateQuery,
+  querySuccessError?: IQuerySuccessError<IPaginateResponse<News>>
+) => {
+  return useQuery(queryKeys().get("NEWS")!, () => getAllNews(params), {
+    onError: querySuccessError?.onError,
+    onSuccess: querySuccessError?.onSuccess,
+  });
+};
---
+++ src/api/news/useGetOneNews.tsx
@@ -0,0 +1,12 @@
+import { useQuery } from "react-query";
+import { queryKeys } from "../../global/queryKeys";
+import { axiosInstance } from "../../helpers/axios";
+import { News } from "../../models/news";
+
+const getOneNews = (id: number) => {
+  return axiosInstance.get<News>(`/news/${id}`);
+};
+
+export const useGetOneNews = (id: number) => {
+  return useQuery(queryKeys().get("NEWS")!, () => getOneNews(id));
+};
