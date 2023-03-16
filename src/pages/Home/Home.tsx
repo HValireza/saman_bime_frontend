f083bca68527034db9de8cc35392dbbbc96aec95
Hosein Hoseiny <seied78@gmail.com>
2023-03-17 01:42:16 +0330

    Added Slider Data to Home

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,11 +4,14 @@ import Reinsurance from "./components/Reinsurance/Reinsurance";
 import "./Home.scss";
 import Recent from "./components/Recent/Recent";
 import Brands from "./components/Brands/Brands";
+import { useGetIntros } from "../../api/intros/useGetIntros";
 
 const Home: React.FC = () => {
+  const { data } = useGetIntros();
+
   return (
     <div className="si-home-container">
-      <Slider />
+      <Slider count={data?.data.count} sliderImages={data?.data.data} />
       <Reinsurance />
       <Services />
       <Recent />
