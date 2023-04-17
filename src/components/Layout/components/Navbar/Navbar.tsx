387a7343b5320291500c75d3fc21d324ab2dbfe7
Hosein Hoseiny <seied78@gmail.com>
2023-04-17 14:10:43 +0330

    Added Navbar Data

--- src/components/Layout/components/Navbar/Navbar.tsx
+++ src/components/Layout/components/Navbar/Navbar.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { useGetCategories } from "../../../../api/categories/useGetCategories";
 import Burger from "./components/Burger/Burger";
 import Expand from "./components/Expand/Expand";
 import Header from "./components/Header/Header";
@@ -12,12 +14,18 @@ interface INavbar {
 }
 
 const Navbar: React.FC<INavbar> = ({ language, setLanguage }) => {
+  const [page, setPage] = useState(1);
+  const [count, setCount] = useState(30);
+
+  const { data } = useGetCategories({ page, count });
+  console.log(data);
+
   return (
     <div className="si-navbar-container">
       <NavbarBG />
       <Header />
       <Burger />
-      <Expand />
+      <Expand data={data?.data.data} />
       <SearchButton />
       <LanguageButton setLanguage={setLanguage} language={language} />
     </div>
