af7c2135140692e586ff7adc8c2258610db49575
Alireza Heydari <alirezahwd@gmail.com>
2023-06-08 15:33:23 +0430

    Fixed card slider data

--- src/pages/Home/components/Reinsurance/Components/CardSlider/CardSlider.tsx
+++ src/pages/Home/components/Reinsurance/Components/CardSlider/CardSlider.tsx
@@ -5,14 +5,31 @@ import "swiper/css/bundle";
 import "./CardSlider.scss";
 import { useNavigate } from "react-router-dom";
 import { useGetSamanGoals } from "../../../../../../api/samanGoals/useGetSamanGoals";
-import Loading from "../../../../../../components/Loading/Loading";
 import { API_BASE_URL } from "../../../../../../global/constans";
+import { useGetFindePageRoute } from "../../../../../../api/pages/useGetFindPageRoute";
+import { useEffect, useState } from "react";
 
 const CardSlider: React.FC = () => {
   const navigate = useNavigate();
-  const navigation = (address?: string) => {
-    scrollTo(0, 0);
-    navigate(address ? address : "/no-page");
+  const [pageId, setPageId] = useState(-1);
+
+  const { refetch } = useGetFindePageRoute(pageId, {
+    onSuccess: (res) => {
+      navigate(res.data.route);
+      window.scrollTo(0, 0);
+    },
+  });
+
+  useEffect(() => {
+    if (pageId !== -1) refetch();
+  }, [pageId]);
+
+  const navigation = (address?: string, page_id?: number) => {
+    if (address) {
+      navigate(address);
+    } else if (page_id) {
+      setPageId(page_id);
+    }
   };
 
   const { data } = useGetSamanGoals();
@@ -38,7 +55,7 @@ const CardSlider: React.FC = () => {
 
               <div
                 className="si-card-slider__button"
-                onClick={() => navigation(d.address)}
+                onClick={() => navigation(d.address, d.page_id)}
               >
                 <div className="si-card-slider__button__text">
                   بیشتر بخوانید
