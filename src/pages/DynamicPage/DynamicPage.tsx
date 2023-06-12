14932f49cc9c33e635659966c7b913353cf87b1b
Alireza Heydari <alirezahwd@gmail.com>
2023-06-12 11:11:40 +0430

    Fixed Dynamic page

--- src/pages/DynamicPage/DynamicPage.tsx
+++ src/pages/DynamicPage/DynamicPage.tsx
@@ -1,13 +1,22 @@
 import { useGetPageByURL } from "../../api/pages/useGetPageByURL";
 import News from "../News/News";
+import Post from "../Post/Post";
 
 export const DynamicPage = () => {
   const url = window.location.pathname;
   const { data } = useGetPageByURL(url);
+  const pageContent = data?.data.content;
 
   if (data?.data.cards?.length) return <div>Cards</div>;
-  if (data?.data.content) return <div>content</div>;
+  if (data?.data.content)
+    return (
+      <Post
+        bannerImage={pageContent?.image}
+        text={pageContent?.text}
+        pdf={pageContent?.pdf}
+      />
+    );
   if (data?.data.news?.length) return <News />;
   if (data?.data.accordions?.length) return <div>accordions</div>;
-  return <div>FUCK OFF</div>;
+  return <div>Fuck you</div>;
 };
