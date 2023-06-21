8384377f38f1efd8963e7d421303dd98e54f6ef9
Alireza Heydari <alirezahwd@gmail.com>
2023-06-21 16:30:42 +0430

    Fixed manager card

--- src/pages/Manager/components/ManagerCard/ManagerCard.tsx
+++ src/pages/Manager/components/ManagerCard/ManagerCard.tsx
@@ -1,15 +1,22 @@
-import { IManager } from "../../../../Mock/Managers/Managers/ManagersMock";
+import { API_BASE_URL } from "../../../../global/constans";
+import { IManager } from "../../../../models/manager";
 import "./ManagerCard.scss";
 
-const ManagerCard: React.FC<IManager> = ({ name, image, position }) => {
+const ManagerCard: React.FC<IManager> = ({
+  title,
+  image,
+  subtitle,
+  address,
+  id,
+}) => {
   const backgroundStyles = {
-    backgroundImage: `linear-gradient(180deg, rgba(13, 48, 110, 0) 50.12%, #0D306E 100%) , url(${image})`,
+    backgroundImage: `linear-gradient(180deg, rgba(13, 48, 110, 0) 50.12%, #0D306E 100%) , url(${API_BASE_URL}/${image})`,
   };
 
   return (
     <div className="si-manager-card" style={backgroundStyles}>
-      <h3 className="si-manager-name">{name}</h3>
-      <h4 className="si-manager-position">{position}</h4>
+      <h3 className="si-manager-name">{title}</h3>
+      <h4 className="si-manager-position">{subtitle}</h4>
     </div>
   );
 };
