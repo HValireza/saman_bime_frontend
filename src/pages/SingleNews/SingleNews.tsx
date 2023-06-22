c294d29796fd1483869843279ea4bf3532f7f854
Alireza Heydari <alirezahwd@gmail.com>
2023-06-22 12:45:55 +0430

    Added news not found

--- src/pages/SingleNews/SingleNews.tsx
+++ src/pages/SingleNews/SingleNews.tsx
@@ -2,10 +2,15 @@ import { useGetAuthor } from "../../api/authors/useGetAuthor";
 import { useGetOneNews } from "../../api/news/useGetOneNews";
 import Post from "../Post/Post";
 import { Author } from "../../models/author";
+import NoPage from "../NoPage/NoPage";
 const SingleNews = () => {
   const newsIdArray = window.location.pathname.split("-");
   const newsId = Number(newsIdArray[newsIdArray.length - 1]);
-  const { data } = useGetOneNews(newsId);
+  const { data, isError } = useGetOneNews(newsId);
+  if (isError) {
+    return <NoPage />;
+  }
+
   const news = data?.data;
 
   return <Post title={news?.title} text={news?.text} image={news?.image} />;
