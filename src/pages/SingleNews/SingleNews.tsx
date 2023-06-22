d3ce3d7082001ee16327ece65cd027b6388548e2
Alireza Heydari <alirezahwd@gmail.com>
2023-06-22 16:58:57 +0430

    Fixed single news page for 404

--- src/pages/SingleNews/SingleNews.tsx
+++ src/pages/SingleNews/SingleNews.tsx
@@ -3,10 +3,14 @@ import { useGetOneNews } from "../../api/news/useGetOneNews";
 import Post from "../Post/Post";
 import { Author } from "../../models/author";
 import NoPage from "../NoPage/NoPage";
+import Loading from "../../components/Loading/Loading";
 const SingleNews = () => {
   const newsIdArray = window.location.pathname.split("-");
   const newsId = Number(newsIdArray[newsIdArray.length - 1]);
-  const { data, isError } = useGetOneNews(newsId);
+  const { data, isLoading, isError } = useGetOneNews(newsId);
+  if (isLoading) {
+    return <Loading />;
+  }
   if (isError) {
     return <NoPage />;
   }
